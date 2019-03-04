15dfe537ef091a278eaf15536f88fc67a97014cc
Jonathan Xu <jonathanj.xu97@gmail.com>
2019-03-03 21:14:17 -0600

    Change to menu drawer, upgrade react version to enable hooks

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,35 +1,87 @@
-import React, { Component } from 'react';
+import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import ClockIcon from '@material-ui/icons/AccessTime';
+import EventIcon from '@material-ui/icons/Event';
+import BallotIcon from '@material-ui/icons/Ballot';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import './styles.scss';
 
-class Nav extends Component {
-  render() {
-    return (
+const SideList = () => {
+  return (
+    <List>
+      <Link to="/">
+        <ListItem button>
+          <ListItemIcon>
+            <ClockIcon />
+          </ListItemIcon>
+          <ListItemText primary="Timeslots" />
+        </ListItem>
+      </Link>
+      <Link to="/schedule">
+        <ListItem button>
+          <ListItemIcon>
+            <EventIcon />
+          </ListItemIcon>
+          <ListItemText primary="My Schedule" />
+        </ListItem>
+      </Link>
+      <Link to="/information">
+        <ListItem button>
+          <ListItemIcon>
+            <BallotIcon />
+          </ListItemIcon>
+          <ListItemText primary="Information" />
+        </ListItem>
+      </Link>
+    </List>
+  );
+}
+
+const Nav = (props) => {
+  const [ open, setOpen ] = useState(false);
+
+  return (
+    <Fragment>
+      <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+        <div
+          tabIndex={0}
+          role="button"
+          onClick={() => setOpen(false)}
+          onKeyDown={() => setOpen(false)}
+        >
+          <SideList />
+        </div>
+      </Drawer>
+
       <AppBar position="static" className="nav">
         <Toolbar>
+          { props.jwt &&
+            <IconButton color="inherit" aria-label="Menu" onClick={() => setOpen(true)}>
+              <MenuIcon />
+            </IconButton>
+          }
+
           <Link to="/" className="grow">
             <Typography variant="h6" color="inherit">
               EOH Volunteer Portal
             </Typography>
           </Link>
-
-          { this.props.jwt &&
-            <>
-              <Link to="/">Timeslots</Link>
-              <Link to="/schedule">Your Schedule</Link>
-              <Link to="/information">Information</Link>
-            </>
-          }
         </Toolbar>
       </AppBar>
-    );
-  }
-}
+    </Fragment>
+  );
+};
 
 const mapStateToProps = state => ({
   jwt: state.user.jwt,
